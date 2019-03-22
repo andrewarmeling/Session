f65b3efe9f56004d696c84b8814dd4839582cadd
André Warmeling Gomes <andre.w.gomes@hotmail.com>
2019-03-21 21:58:55 -0300

    Implementação de DAO

---
+++ src/br/com/otSession/jdbc/JDBCUsuarioDAO.java
@@ -0,0 +1,35 @@
+package br.com.otSession.jdbc;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCUsuarioDAO {
+
+	private Connection conexao;
+
+	public JDBCUsuarioDAO(Connection conexao) {
+		this.conexao = conexao;
+	}
+
+	public boolean validarUsuario(String usuario) {
+		try {
+
+			String comando = "SELECT usuario FROM usuarios WHERE usuario = " + usuario + ";";
+			Statement stmt = conexao.createStatement();
+			ResultSet rs = stmt.executeQuery(comando);
+
+			if (rs.next()) {
+				return true;
+			}
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+}
---
+++ src/br/com/otSession/loginservlet/LoginServlet.java
@@ -0,0 +1,22 @@
+package br.com.otSession.loginservlet;
+
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+}
